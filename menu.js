d60a8f73e9d80ea0ecc0f1bd8608aa7b54439cfc
pch6828 <pch6828@hanyang.ac.kr>
2021-01-28 20:01:32 +0900

    scroll alphabet done, maybe change this to div

---
+++ menu.js
@@ -0,0 +1,47 @@
+import {Slot} from './slot.js?ver=1';
+
+export class Menu{
+    constructor(x, y, slotsize){
+        this.x = x;
+        this.y = y;
+        this.nowx = x;
+        this.centerx = x;
+        this.slotsize = slotsize;
+        this.slot = [];
+        const upper = 65;
+        const lower = 97;
+        for(let i = 0; i < 26; i++){
+            this.slot[i] = new Slot(x+i*slotsize, y, slotsize, String.fromCharCode(upper+i, lower+i), null);
+        }
+    }
+
+    animate(ctx, moveX){
+        const movement = moveX*0.7;
+        this.nowx+=movement;  
+        let overflow = false;
+        let font_size = 100;
+        
+        if(this.centerx>this.nowx+25*this.slotsize){
+            this.nowx = this.centerx-25*this.slotsize;
+            overflow = true;
+        }else if(this.centerx<this.nowx){
+            this.nowx = this.centerx;
+            overflow = true;
+        }
+
+        for(let i = 0; i < this.slot.length; i++){
+            if(!overflow){
+                this.slot[i].x += movement;
+            }
+            this.slot[i].animate(ctx);
+        }
+    }
+
+    click(x, y){
+        for(let i = 0; i < this.slot.length; i++){
+            if(this.slot[i].collide(x, y)){
+                //
+            }
+        }
+    }
+}
---
+++ slot.js
@@ -0,0 +1,22 @@
+export class Slot{
+    constructor(x, y, size, alpha, description){
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.alpha = alpha;
+        this.description = description;
+    }
+
+    animate(ctx){
+        ctx.fillText(this.alpha, this.x, this.y, this.size);
+    }
+
+    collide(x, y){
+        if(x>=this.x-this.size/4&&x<=this.x+this.size/4&&y>=this.y-this.size/2&&y<=this.y){
+            //console.log(this.alpha);
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
