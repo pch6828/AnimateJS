75b49fad5a539d5950e7bebc77bb9ca9d8ffe715
pch6828 <pch6828@gmail.com>
2024-05-19 03:05:21 +0900

    Setting: route setting done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
-
+import Main from './animate-js/main/main.jsx'
+import A_ from './animate-js/content/A_.jsx'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path='/' Component={Main} />
+        <Route exact path='/A_' Component={A_} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/animate-js/content/A_.jsx
@@ -0,0 +1,9 @@
+function A_() {
+    return (
+        <div>
+            Content page for 'A'
+        </div>
+    );
+}
+
+export default A_;
