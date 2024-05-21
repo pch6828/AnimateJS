5b6468d600265588b3cefb70e74894692e9b37e3
pch6828 <pch6828@gmail.com>
2024-05-21 10:49:52 +0900

    content page: testing animation

---
+++ src/animate-js/class/animate/c_coffee.jsx
@@ -0,0 +1,9 @@
+var length = 50;
+
+function AnimationC(ctx, width, height) {
+    ctx.fillStyle = 'rbga(0,0,0,1)';
+    ctx.fillRect(0, 0, width / 3, width / 3);
+    // length = ((length + 1) % 50);/
+}
+
+export default AnimationC;
--- src/animate-js/class/content_page/content.jsx
+++ src/animate-js/class/content_page/content.jsx
@@ -2,6 +2,8 @@ import { useRef, useState, useEffect } from 'react';
 
 import '../../style/content.css';
 
+import AnimationC from '../animate/c_coffee';
+
 function Content() {
     const canvasRef = useRef(null);
     const contextRef = useRef(null);
@@ -12,16 +14,40 @@ function Content() {
     useEffect(() => {
         const canvas = canvasRef.current;
         const context = canvas.getContext('2d');
-        canvas.width = window.innerWidth * 0.7;
-        canvas.height = window.innerHeight;
-        console.log(canvas.width, canvas.height);
+        canvas.width = devicePixelRatio * window.innerWidth * 0.7;
+        canvas.height = devicePixelRatio * window.innerHeight;
 
         context.strokeStyle = 'black';
         context.lineWidth = 2.5;
         contextRef.current = context;
 
+        let requestId;
+
+        function resizeCanvas() {
+            const canvas = canvasRef.current;
+            canvas.width = devicePixelRatio * window.innerWidth * 0.7;
+            canvas.height = devicePixelRatio * window.innerHeight;
+        }
+
+        window.addEventListener('resize', resizeCanvas)
+
+        const requestAnimation = () => {
+            requestId = window.requestAnimationFrame(requestAnimation);
+
+            if (ctx) {
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                AnimationC(ctx, canvas.width, canvas.height);
+            }
+        };
+
+        requestAnimation();
+
         setCtx(context);
-    }, []);
+
+        return () => {
+            window.cancelAnimationFrame(requestId);
+        };
+    }, [ctx]);
 
     function startDrawing() {
         setIsDrawing(true);
@@ -35,7 +61,6 @@ function Content() {
         const { offsetX, offsetY } = nativeEvent;
         if (ctx) {
             if (!isDrawing) {
-                console.log(nativeEvent);
                 ctx.beginPath();
                 ctx.moveTo(offsetX, offsetY);
             } else {
