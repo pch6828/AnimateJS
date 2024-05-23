bbc2db1b68b33d7ce454143262e09cc168b774ed
pch6828 <pch6828@gmail.com>
2024-05-23 12:42:34 +0900

    main-menu: deactivate un-implemented slots

--- src/animate-js/class/main_menu/wheel_button_item.jsx
+++ src/animate-js/class/main_menu/wheel_button_item.jsx
@@ -16,7 +16,7 @@ const items = [
     },
     {
         key: 'D',
-        title: 'Design? Develop?',
+        title: '', //'Design? Develop?'
         color: '#FF6900'
     },
     {
@@ -36,7 +36,7 @@ const items = [
     },
     {
         key: 'H',
-        title: '',
+        title: '', //'Homebody',
         color: '#F6B700'
     },
     {
@@ -51,7 +51,7 @@ const items = [
     },
     {
         key: 'K',
-        title: 'Kidult',
+        title: '', //'Kidult',
         color: '#30B700'
     },
     {
@@ -76,7 +76,7 @@ const items = [
     },
     {
         key: 'P',
-        title: 'Postgraduate',
+        title: '', //'Postgraduate',
         color: '#2AD2C9'
     },
     {
