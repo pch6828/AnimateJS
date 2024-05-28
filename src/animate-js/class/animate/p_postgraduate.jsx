6656fa5a61f29a90e3f55c4a7228a533190efbc6
pch6828 <pch6828@gmail.com>
2024-05-28 14:24:09 +0900

    content-P: add throwing motion to graduation cap

--- src/animate-js/class/animate/p_postgraduate.jsx
+++ src/animate-js/class/animate/p_postgraduate.jsx
@@ -4,10 +4,60 @@ class GraduationCap {
         this.yRatio = yRatio;
         this.widthRatio = 0.2;
         this.heightRatio = 0.1;
+        this.selectedPos = null;
+        this.prevIsDown = false;
+        this.throw = false;
+        this.throwVector = null;
+        this.angle = 0;
     }
 
-    move() {
+    move(movement, width, height) {
+        const currentPos = { x: width * this.xRatio, y: height * this.yRatio };
+        const capWidth = this.widthRatio * width / 3.5 * 2;
+        const capHeight = this.heightRatio * width;
+        if (movement.isDown) {
+            if (!this.prevIsDown &&
+                currentPos.x - capWidth / 2 <= movement.mousePoint.x &&
+                movement.mousePoint.x <= currentPos.x + capWidth / 2 &&
+                currentPos.y - capHeight / 2 <= movement.mousePoint.y &&
+                movement.mousePoint.y <= currentPos.y + capHeight / 2) {
+                this.throw = false;
+                this.selectedPos = { x: movement.mousePoint.x, y: movement.mousePoint.y };
+            }
+            if (this.selectedPos) {
+                this.throwVector = { dx: 0, dy: 0 };
+                this.throwVector.dx = (movement.mousePoint.x - this.selectedPos.x) / width;
+                this.throwVector.dy = (movement.mousePoint.y - this.selectedPos.y) / height;
 
+                this.xRatio += (movement.mousePoint.x - this.selectedPos.x) / width;
+                this.yRatio += (movement.mousePoint.y - this.selectedPos.y) / height;
+                const throwVectorSize = Math.sqrt(this.throwVector.dx * this.throwVector.dx + this.throwVector.dy * this.throwVector.dy);
+                this.angle += this.throwVector.dx * (isNaN(throwVectorSize) ? 1 : throwVectorSize) * 100;
+                this.selectedPos = { x: movement.mousePoint.x, y: movement.mousePoint.y };
+            }
+        } else {
+            this.selectedPos = null;
+        }
+
+        if (this.prevIsDown && this.throwVector && !this.selectedPos) {
+            this.throw = true;
+        }
+        this.prevIsDown = movement.isDown;
+
+        if (this.throw) {
+            this.xRatio += this.throwVector.dx / 10;
+            this.yRatio += this.throwVector.dy / 10;
+            this.throwVector.dy += 0.0015;
+            this.angle *= 1.01;
+        }
+
+        if (this.xRatio > 1.1 || this.xRatio < -0.1 || this.yRatio > 1.1 || this.yRatio < -0.1) {
+            this.throw = false;
+            this.xRatio = 0.5;
+            this.yRatio = 0.6;
+            this.throwVector = null;
+            this.angle = 0;
+        }
     }
 
     draw(ctx, width, height) {
@@ -15,30 +65,32 @@ class GraduationCap {
         ctx.globalCompositeOperation = 'source-over';
 
         ctx.fillStyle = 'rgba(30,44,74,1)';
-        ctx.translate(width * this.xRatio, height * this.yRatio)
+        ctx.translate(width * this.xRatio, height * this.yRatio);
+        ctx.rotate(this.angle);
+
         ctx.beginPath();
         ctx.moveTo(0, 0);
-        ctx.lineTo(-width * this.widthRatio / 3, -width * this.heightRatio / 6);
-        ctx.lineTo(-width * this.widthRatio / 3.5, -width * this.heightRatio / 1.5);
-        ctx.lineTo(0, -width * this.heightRatio / 2);
+        ctx.lineTo(-width * this.widthRatio / 3.5, -width * this.heightRatio / 6);
+        ctx.lineTo(-width * this.widthRatio / 3, width * this.heightRatio / 3.5);
+        ctx.lineTo(0, width * this.heightRatio / 2);
         ctx.closePath();
         ctx.fill();
 
         ctx.fillStyle = 'rgba(8,19,43,1)';
         ctx.beginPath();
         ctx.moveTo(0, 0);
-        ctx.lineTo(width * this.widthRatio / 3, -width * this.heightRatio / 6);
-        ctx.lineTo(width * this.widthRatio / 3.5, -width * this.heightRatio / 1.5);
-        ctx.lineTo(0, -width * this.heightRatio / 2);
+        ctx.lineTo(width * this.widthRatio / 3.5, -width * this.heightRatio / 6);
+        ctx.lineTo(width * this.widthRatio / 3, width * this.heightRatio / 3.5);
+        ctx.lineTo(0, width * this.heightRatio / 2);
         ctx.closePath();
         ctx.fill();
 
         ctx.fillStyle = 'rgba(35,56,114,1)';
         ctx.beginPath();
-        ctx.moveTo(0, -width * this.heightRatio / 2);
-        ctx.lineTo(width * this.widthRatio / 3.5 * 2, -width * this.heightRatio / 4 * 3);
-        ctx.lineTo(0, -width * this.heightRatio);
-        ctx.lineTo(-width * this.widthRatio / 3.5 * 2, -width * this.heightRatio / 4 * 3);
+        ctx.moveTo(0, 0);
+        ctx.lineTo(width * this.widthRatio / 3.5 * 2, -width * this.heightRatio / 4);
+        ctx.lineTo(0, -width * this.heightRatio / 2);
+        ctx.lineTo(-width * this.widthRatio / 3.5 * 2, -width * this.heightRatio / 4);
         ctx.closePath();
         ctx.fill();
 
@@ -61,9 +113,10 @@ function AnimationP(ctx, width, height, movement) {
     ctx.fillText('Postgraduate', centerx - textwidth / 2, centery + fontSize * 1.5);
 
     if (graudationCap === null) {
-        graudationCap = new GraduationCap(0.5, 0.65);
+        graudationCap = new GraduationCap(0.5, 0.6);
     }
 
+    graudationCap.move(movement, width, height);
     graudationCap.draw(ctx, width, height);
     // 학사모를 잡아 던질 수 있으며
     // 한번 던진 학사모는 글씨 뒤로 떨어지도록
