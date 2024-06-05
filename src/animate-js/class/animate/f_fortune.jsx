921d7573f5587322f8cde8947cd220598d9099b1
pch6828 <pch6828@gmail.com>
2024-06-05 10:20:50 +0900

    content-F: add fortune cookie

--- src/animate-js/class/animate/f_fortune.jsx
+++ src/animate-js/class/animate/f_fortune.jsx
@@ -1,12 +1,61 @@
+class fortunePaper {
+
+};
+
 function AnimationF(ctx, width, height, movement) {
-    // 포춘쿠키
-    // 원형으로 Fortune 글씨 쓰고
-    // 가운데에 포춘쿠키
-    // 살짝 튀어나온 쪽지
-    // 당기면 나오도록
-    // 랜덤 메시지 추가
-    // 다 당겨서 쪽지가 나오면 쿠키가 아래로 떨어지고
-    // 새 쿠키 스폰 
+    const centerx = width / 2;
+    const centery = height / 2;
+    const cookieSize = width / 10;
+
+    ctx.globalCompositeOperation = 'source-over';
+    ctx.lineCap = 'round';
+    ctx.lineJoin = 'round';
+    ctx.lineWidth = width / 30;
+
+    ctx.save();
+    ctx.translate(centerx, centery);
+    ctx.rotate(Math.PI / 6);
+
+    ctx.save();
+    ctx.rotate(Math.PI / 3);
+    ctx.fillStyle = "rgba(255, 170, 77, 1)";
+    ctx.strokeStyle = "rgba(255, 170, 77, 1)";
+
+    ctx.beginPath();
+    ctx.moveTo(0, -cookieSize);
+    ctx.lineTo(-cookieSize / 2 * Math.sqrt(3), -cookieSize / 2);
+    ctx.arcTo(-cookieSize * Math.sqrt(3), cookieSize, 0, cookieSize, cookieSize);
+    ctx.closePath();
+
+    ctx.stroke();
+    ctx.fill();
+
+    ctx.restore();
+
+    ctx.save();
+    ctx.fillStyle = "rgba(252, 156, 36, 1)";
+    ctx.strokeStyle = "rgba(252, 156, 36, 1)";
+
+    ctx.beginPath();
+    ctx.moveTo(0, -cookieSize);
+    ctx.lineTo(cookieSize / 2 * Math.sqrt(3), -cookieSize / 2);
+    ctx.arcTo(cookieSize * Math.sqrt(3), cookieSize, 0, cookieSize, cookieSize);
+    ctx.closePath();
+
+    ctx.stroke();
+    ctx.fill();
+
+    ctx.strokeStyle = "rgba(136, 61, 36, 1)";
+    ctx.lineWidth = ctx.lineWidth / 2;
+    ctx.beginPath();
+    ctx.moveTo(cookieSize / 2 * Math.sqrt(3), -cookieSize / 2);
+    ctx.arcTo(cookieSize * Math.sqrt(3), cookieSize, 0, cookieSize, cookieSize);
+    ctx.stroke();
+    ctx.closePath();
+
+    ctx.restore();
+
+    ctx.restore();
 }
 
 export default AnimationF;
