eb8b9d8d4d25f06af6d97800a9e1bfefdf4f48ca
pch6828 <pch6828@gmail.com>
2024-06-09 20:41:16 +0900

    transition: add transition effect

--- src/animate-js/class/transition/transition_wrapper.jsx
+++ src/animate-js/class/transition/transition_wrapper.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 import '../../style/transition.css'
 
@@ -9,15 +10,25 @@ import Content from '../content_page/content.jsx';
 function TransitionWrapper() {
     const [deg, setDeg] = useState(0);
 
+    const location = useLocation();
+
     function updateDeg(newDeg) {
         setDeg(newDeg);
     }
 
     return (
-        <Routes>
-            <Route path='/' element={<Main deg={deg} setDeg={updateDeg} />} />
-            <Route path='/Content/:id' element={<Content />} />
-        </Routes>
+        <TransitionGroup>
+            <CSSTransition
+                key={location.pathname}
+                timeout={10000}
+                className="page-transition"
+            >
+                <Routes location={location}>
+                    <Route path='/' element={<Main deg={deg} setDeg={updateDeg} />} />
+                    <Route path='/Content/:id' element={<Content />} />
+                </Routes>
+            </CSSTransition>
+        </TransitionGroup>
     );
 }
 
