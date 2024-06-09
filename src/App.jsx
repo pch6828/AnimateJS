3ed4e133262d669539166aa6a80cee6168c5b7ae
pch6828 <pch6828@gmail.com>
2024-06-09 15:25:36 +0900

    content-page: adjust css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import { Routes, Route, HashRouter } from 'react-router-dom';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 import Main from './animate-js/class/main_menu/main.jsx'
 import Content from './animate-js/class/content_page/content.jsx';
@@ -14,18 +13,11 @@ function App() {
 
   return (
     <HashRouter>
-      <TransitionGroup className="transitions-wrapper">
-        <CSSTransition
-          classNames={"right"}
-          timeout={300}
-        >
-          <Routes>
-            <Route path='/' element={<Main deg={deg} setDeg={updateDeg} />} />
-            <Route path='/Content/:id' element={<Content />} />
-          </Routes>
-        </CSSTransition>
-      </TransitionGroup >
-    </HashRouter>
+      <Routes>
+        <Route path='/' element={<Main deg={deg} setDeg={updateDeg} />} />
+        <Route path='/Content/:id' element={<Content />} />
+      </Routes>
+    </HashRouter >
   );
 }
 
