a40dba2bfec947550ec274144d2a7bf7598c219c
pch6828 <pch6828@gmail.com>
2024-06-10 10:13:00 +0900

    content-K: new concept

--- src/animate-js/class/animate/k_kidult.jsx
+++ src/animate-js/class/animate/k_kidult.jsx
@@ -1,5 +1,9 @@
 function AnimationK(ctx, width, height, movement) {
-    // 레고?
+    // 레고  
+    // KID, ADULT로 레고 블록 
+    // 회색 OR 검은색으로 플레이트
+    // 조립 기능 구현, 블록 버리기 기능 구현
+    // 블록을 모두 아래로 버릴 경우 위에서 블럭 떨어트리기
 }
 
 export default AnimationK;
