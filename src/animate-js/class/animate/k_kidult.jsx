fa7809bcb8551c6f2dd8e379abab2786f5b549ee
pch6828 <pch6828@gmail.com>
2024-06-11 21:26:25 +0900

    content-K: lego feature 1 - stud aligned block

--- src/animate-js/class/animate/k_kidult.jsx
+++ src/animate-js/class/animate/k_kidult.jsx
@@ -10,55 +10,81 @@ class Stud {
 
     }
 
-    draw(ctx, x, y) {
-        ctx.globalCompositeOperation = 'source-over';
-        ctx.fillStyle = this.color;
+    draw(ctx, x, y, width, height) {
+        if (this.connection) {
+            this.connection.draw(ctx, x, y, width, height, this);
+        }
+        else {
+            ctx.globalCompositeOperation = 'source-over';
+            ctx.fillStyle = this.color;
 
-        ctx.beginPath();
-        ctx.roundRect(x - Stud.studWidth / 2, y - Stud.studWidth / 2, Stud.studWidth, Stud.studWidth, Stud.studWidth / 4)
-        ctx.closePath();
-        ctx.fill();
+            ctx.beginPath();
+            ctx.roundRect(x - Stud.studWidth / 2, y - Stud.studWidth / 2, Stud.studWidth, Stud.studWidth, Stud.studWidth / 4)
+            ctx.closePath();
+            ctx.fill();
+        }
     }
 };
 
-const BlockStatus = {
-    spawn: 'spawn',
-    moving: 'moving',
-    stable: 'stable',
-    falling: 'falling'
-};
-
 class LegoBlock {
-    constructor(letter) {
+    constructor(letter, blockColor, studColor) {
         this.size = Math.floor(Math.random() * 3) + 1;
         this.letter = letter;
+        this.color = blockColor;
+        this.drawed = false;
         this.studs = [];
-        this.antistuds = [];
+        this.antiStuds = [];
+
         for (let i = 0; i < this.size; i++) {
-            this.studs[i] = new Stud(this);
-            this.antistuds[i] = null;
+            this.studs[i] = new Stud(this, studColor);
+            this.antiStuds[i] = null;
         }
-        this.spawning = true;
-        this.status = BlockStatus.spawn;
+
+        this.selectPos = null;
     }
 
     move(movement, width, height) {
 
     }
 
-    draw(ctx, width, height) {
+    draw(ctx, x, y, width, height, stud) {
+        const blockWidth = Stud.studWidth * 2;
+        const blockHeight = Stud.studWidth * 3;
+        const antiStudIdx = this.antiStuds.indexOf(stud);
+
+        if (this.drawed) {
+            return;
+        }
+
+        this.drawed = true;
+
+        for (let i = 0; i < this.size; i++) {
+            const stud = this.studs[i];
+            stud.draw(ctx, x - blockWidth * antiStudIdx + blockWidth * i, y - blockHeight, width, height);
+        }
+
         ctx.globalCompositeOperation = 'source-over';
+        ctx.fillStyle = this.color;
+
+        ctx.fillRect(x - blockWidth * antiStudIdx - blockWidth / 2, y, blockWidth * this.size, -blockHeight);
+    }
+
+    connect(stud) {
+
     }
 };
 
 const blocks = [];
-const basePlate = [];
+const basePlateStud = [];
 
 function AnimationK(ctx, width, height, movement) {
     const centerx = width / 2;
     const centery = height / 2;
     const basePlateLength = 21; // max block length * # of letters
-    const basePlateColor = "rgba(66,73,82,1)";
+    const basePlateColor = {
+        studColor: "rgba(96,103,112,1)",
+        blockColor: "rgba(66,73,82,1)"
+    };
 
     Stud.studWidth = width / 50;
     // 레고  
@@ -68,7 +94,7 @@ function AnimationK(ctx, width, height, movement) {
     // 블록을 버릴 경우 위에서 해당 글자의 블럭 떨어트리기
 
     if (blocks.length === 0) {
-        blocks[0] = new LegoBlock('A');
+        blocks[0] = new LegoBlock('A', 'red', 'red');
         blocks[1] = new LegoBlock('D');
         blocks[2] = new LegoBlock('U');
         blocks[3] = new LegoBlock('L');
@@ -77,20 +103,30 @@ function AnimationK(ctx, width, height, movement) {
         blocks[6] = new LegoBlock('I');
     }
 
-    if (basePlate.length === 0) {
+    if (basePlateStud.length === 0) {
         for (let i = 0; i < basePlateLength; i++) {
-            basePlate[i] = new Stud(null, basePlateColor);
+            basePlateStud[i] = new Stud(null, basePlateColor.studColor);
         }
     }
 
+    for (let i = 0; i < blocks[0].size; i++) {
+        basePlateStud[i].connection = blocks[0];
+        blocks[0].antiStuds[i] = basePlateStud[i];
+    }
+
     for (let i = 0; i < basePlateLength; i++) {
-        basePlate[i].draw(ctx, centerx - (10 - i) * Stud.studWidth * 2, centery * 1.3);
+        const x = centerx - (10 - i) * Stud.studWidth * 2;
+        const y = centery * 1.3;
+        basePlateStud[i].draw(ctx, x, y, width, height);
     }
 
     ctx.globalCompositeOperation = 'source-over';
-    ctx.fillStyle = basePlateColor;
+    ctx.fillStyle = basePlateColor.blockColor;
     ctx.fillRect(centerx - 21 * Stud.studWidth, centery * 1.3, 42 * Stud.studWidth, Stud.studWidth);
 
+    for (let i = 0; i < blocks.length; i++) {
+        blocks[i].drawed = false;
+    }
 }
 
 export default AnimationK;
