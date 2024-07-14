95f9c093831fc76ebb377475683961a578829d45
pch6828 <pch6828@gmail.com>
2024-07-14 15:19:47 +0900

    content-W: add color box

--- src/animate-js/class/animate/w_workaholic.jsx
+++ src/animate-js/class/animate/w_workaholic.jsx
@@ -3,6 +3,7 @@ class FallingObject {
         this.posRatio = { xRatio: Math.random(), yRatio: 0 };
         this.dyRatio = 0;
         this.ddyRatio = Math.random() * 0.0003;
+        this.scale = Math.random() * 0.2 + 0.8;
     }
 
     move(movement, width, height) {
@@ -16,6 +17,63 @@ class FallingObject {
         ctx.fillStyle = 'white';
         ctx.fillRect(-width / 60, -width / 60, width / 30, width / 30);
         ctx.restore();
+    }
+}
+
+class PaperPile extends FallingObject {
+    draw(ctx, width, height) {
+
+    }
+}
+
+class ColorBox extends FallingObject {
+    static widthRatio = 0.1;
+    static heightRatio = 0.07;
+    constructor() {
+        super();
+
+        const randomCode = Math.random();
+        this.color = randomCode < 0.33 ? '#ED1D24' : (randomCode > 0.66 ? '#F08F00' : '#77DD77');
+    }
+
+    draw(ctx, width, height) {
+        const boxWidth = width * ColorBox.widthRatio;
+        const boxHeight = width * ColorBox.heightRatio;
+        const boxFrontWidthRatio = 0.7;
+        const boxHandleRatio = 0.3;
+
+        ctx.save();
+        ctx.translate(width * this.posRatio.xRatio, height * this.posRatio.yRatio);
+        ctx.scale(this.scale, this.scale);
+        ctx.globalCompositeOperation = 'source-over';
+
+        ctx.fillStyle = this.color;
+        ctx.fillRect(-boxWidth / 2, -boxHeight / 2, boxWidth, boxHeight);
+
+        ctx.fillStyle = 'rgba(0,0,0,0.3)';
+        ctx.fillRect(
+            -boxWidth / 2 + boxWidth * boxFrontWidthRatio, -boxHeight / 2,
+            boxWidth * (1 - boxFrontWidthRatio), boxHeight
+        );
+
+        ctx.globalCompositeOperation = 'destination-out';
+        ctx.lineJoin = 'round';
+        ctx.lineCap = 'round';
+        ctx.lineWidth = boxHeight / 15;
+        ctx.beginPath();
+        ctx.moveTo(-boxWidth / 2 + boxWidth * boxFrontWidthRatio / 2 * (1 - boxHandleRatio), -boxHeight / 2 * 0.7);
+        ctx.lineTo(-boxWidth / 2 + boxWidth * boxFrontWidthRatio / 2 * (1 + boxHandleRatio), -boxHeight / 2 * 0.7);
+        ctx.closePath();
+        ctx.stroke();
+
+        ctx.restore();
+
+    }
+}
+
+class ColorFile extends FallingObject {
+    draw(ctx, width, height) {
+
     }
 }
 
@@ -42,7 +100,7 @@ class MouseChasingWord {
 }
 
 const mouseChasingWord = new MouseChasingWord('Workaholic');
-const temp = new FallingObject();
+const temp = new ColorBox();
 
 function AnimationW(ctx, width, height, movement) {
 
