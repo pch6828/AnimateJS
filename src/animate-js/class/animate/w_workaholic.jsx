1fac0842990c10c954d5a6d517c58ae92a0827bf
pch6828 <pch6828@gmail.com>
2024-07-14 13:26:29 +0900

    content-W: add prototype for falling object

--- src/animate-js/class/animate/w_workaholic.jsx
+++ src/animate-js/class/animate/w_workaholic.jsx
@@ -1,7 +1,28 @@
+class FallingObject {
+    constructor() {
+        this.posRatio = { xRatio: Math.random(), yRatio: 0 };
+        this.dyRatio = 0;
+        this.ddyRatio = Math.random() * 0.0003;
+    }
+
+    move(movement, width, height) {
+        this.posRatio.yRatio += this.dyRatio;
+        this.dyRatio += this.ddyRatio;
+    }
+
+    draw(ctx, width, height) {
+        ctx.save();
+        ctx.translate(width * this.posRatio.xRatio, height * this.posRatio.yRatio);
+        ctx.fillStyle = 'white';
+        ctx.fillRect(-width / 60, -width / 60, width / 30, width / 30);
+        ctx.restore();
+    }
+}
+
 class MouseChasingWord {
-    constructor(word) {
+    constructor(word, x, y) {
         this.word = word;
-        this.point = { x: 0, y: 0 };
+        this.point = { x: x, y: y };
     }
 
     move(movement, width, height) {
@@ -21,15 +42,17 @@ class MouseChasingWord {
 }
 
 const mouseChasingWord = new MouseChasingWord('Workaholic');
+const temp = new FallingObject();
 
 function AnimationW(ctx, width, height, movement) {
-    const centerx = width / 2;
-    const centery = height / 2;
 
     // 일이 하늘에서 천천히 떨어지고 마우스 위에 Workaholic 글자가 따라다니도록
     // 마우스가 움직임에 따라 글자 위에 쌓인 일들이 흔들리고, 일정 이상이 될 경우 다시 떨어지도록
     mouseChasingWord.move(movement, width, height);
     mouseChasingWord.draw(ctx, width, height);
+
+    temp.move(movement, width, height);
+    temp.draw(ctx, width, height);
 }
 
 export default AnimationW;
