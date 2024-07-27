d2f398948c6bc317cb74761e7b447bcf707c7e81
pch6828 <pch6828@gmail.com>
2024-07-27 14:30:42 +0900

    content-R: design new content

--- src/animate-js/class/animate/r_rambler.jsx
+++ src/animate-js/class/animate/r_rambler.jsx
@@ -1,4 +1,7 @@
 export function AnimationR(ctx, width, height, movement) {
+    // 좌우에서 걸어다니는 글자가 일정 간격으로 나옴 (원형 몸통과 다리 두개, 몸통에 글자 적혀있도록)
+    // 다리 길이, 속도 등은 랜덤
+    // 글자를 마우스로 잡을 수 있고, 이를 가지고 움직이다가 떨어트릴 수 있음
 }
 
 export function CleanR() {
