2705e71e37329d710255816ada1358233ad355de
pch6828 <pch6828@gmail.com>
2026-03-25 16:16:27 +0900

    3/25: comment out all content

--- src/animate-js/class/main_menu/wheel_button_item.jsx
+++ src/animate-js/class/main_menu/wheel_button_item.jsx
@@ -1,179 +1,178 @@
 const items = [
-    {
-        key: 'A',
-        title: 'Algorithm',
-        date: '2024-05-22',
-        hasDetailPage: true,
-        supportPC: true,
-        supportMobile: true
-    },
-    {
-        key: 'B',
-        title: '',
-        hasDetailPage: false
-    },
-    {
-        key: 'C',
-        title: 'Coffee',
-        date: '2024-05-21',
-        hasDetailPage: true,
-        supportPC: true,
-        supportMobile: true
-    },
-    {
-        key: 'D',
-        title: 'Design? Develop?',
-        date: '2024-06-03',
-        hasDetailPage: true,
-        supportPC: true,
-        supportMobile: false
-    },
-    {
-        key: 'E',
-        title: 'Encyclopedia',
-        date: '2024-06-25',
-        hasDetailPage: true,
-        supportPC: true,
-        supportMobile: true
-    },
-    {
-        key: 'F',
-        title: 'Fortune',
-        date: '2024-06-12',
-        hasDetailPage: true,
-        supportPC: true,
-        supportMobile: true
-    },
-    {
-        key: 'G',
-        title: '',//'Geek',
-        hasDetailPage: false
-    },
-    {
-        key: 'H',
-        title: 'Homebody',
-        date: '2024-05-24',
-        hasDetailPage: true,
-        supportPC: true,
-        supportMobile: true
-    },
-    {
-        key: 'I',
-        title: '', //'Introvert',
-        hasDetailPage: false
-    },
-    {
-        key: 'J',
-        title: 'mbti-J',
-        date: '2024-07-11',
-        hasDetailPage: true,
-        supportPC: true,
-        supportMobile: true
-    },
-    {
-        key: 'K',
-        title: 'Kidult',
-        date: '2024-06-14',
-        hasDetailPage: true,
-        supportPC: true,
-        supportMobile: true
-    },
-    {
-        key: 'L',
-        title: '',
-        hasDetailPage: false
-    },
-    {
-        key: 'M',
-        title: 'Mentor',
-        date: '2024-07-25',
-        hasDetailPage: true,
-        supportPC: true,
-        supportMobile: true
-    },
-    {
-        key: 'N',
-        title: '',
-        hasDetailPage: false
-    },
-    {
-        key: 'O',
-        title: '',
-        hasDetailPage: false
-    },
-    {
-        key: 'P',
-        title: 'Postgraduate',
-        date: '2024-05-30',
-        hasDetailPage: true,
-        supportPC: true,
-        supportMobile: true
-    },
-    {
-        key: 'Q',
-        title: '',
-        hasDetailPage: false
-    },
-    {
-        key: 'R',
-        title: 'Rambler',
-        date: '2024-07-30',
-        hasDetailPage: true,
-        supportPC: true,
-        supportMobile: true
-    },
-    {
-        key: 'S',
-        title: 'Steady',
-        date: '2024-08-XX',
-        hasDetailPage: true,
-        supportPC: true,
-        supportMobile: true
-    },
-    {
-        key: 'T',
-        title: 'Tracing',
-        date: '2024-07-18',
-        hasDetailPage: true,
-        supportPC: true,
-        supportMobile: false
-    },
-    {
-        key: 'U',
-        title: '',
-        hasDetailPage: false
-    },
-    {
-        key: 'V',
-        title: 'Versatile',
-        date: '2024-06-01',
-        hasDetailPage: true,
-        supportPC: true,
-        supportMobile: true
-    },
-    {
-        key: 'W',
-        title: 'Workaholic',
-        date: '2024-07-16',
-        hasDetailPage: true,
-        supportPC: true,
-        supportMobile: true
-    },
-    {
-        key: 'X',
-        title: '',
-        hasDetailPage: false
-    },
-    {
-        key: 'Y',
-        title: '',// 'YOLO',
-        hasDetailPage: false
-    },
-    {
-        key: 'Z',
-        title: '', //'Zzz...',
-        hasDetailPage: false
-    },
+    { key: 'A', title: '', hasDetailPage: false },
+    { key: 'B', title: '', hasDetailPage: false },
+    { key: 'C', title: '', hasDetailPage: false },
+    { key: 'D', title: '', hasDetailPage: false },
+    { key: 'E', title: '', hasDetailPage: false },
+    { key: 'F', title: '', hasDetailPage: false },
+    { key: 'G', title: '', hasDetailPage: false },
+    { key: 'H', title: '', hasDetailPage: false },
+    { key: 'I', title: '', hasDetailPage: false },
+    { key: 'J', title: '', hasDetailPage: false },
+    { key: 'K', title: '', hasDetailPage: false },
+    { key: 'L', title: '', hasDetailPage: false },
+    { key: 'M', title: '', hasDetailPage: false },
+    { key: 'N', title: '', hasDetailPage: false },
+    { key: 'O', title: '', hasDetailPage: false },
+    { key: 'P', title: '', hasDetailPage: false },
+    { key: 'Q', title: '', hasDetailPage: false },
+    { key: 'R', title: '', hasDetailPage: false },
+    { key: 'S', title: '', hasDetailPage: false },
+    { key: 'T', title: '', hasDetailPage: false },
+    { key: 'U', title: '', hasDetailPage: false },
+    { key: 'V', title: '', hasDetailPage: false },
+    { key: 'W', title: '', hasDetailPage: false },
+    { key: 'X', title: '', hasDetailPage: false },
+    { key: 'Y', title: '', hasDetailPage: false },
+    { key: 'Z', title: '', hasDetailPage: false }
 ];
+
+// const items = [
+//     {
+//         key: 'A',
+//         title: 'Algorithm',
+//         date: '2024-05-22',
+//         hasDetailPage: true
+//     },
+//     {
+//         key: 'B',
+//         title: '',
+//         hasDetailPage: false
+//     },
+//     {
+//         key: 'C',
+//         title: 'Coffee',
+//         date: '2024-05-21',
+//         hasDetailPage: true
+//     },
+//     {
+//         key: 'D',
+//         title: 'Design? Develop?',
+//         date: '2024-06-03',
+//         hasDetailPage: true
+//     },
+//     {
+//         key: 'E',
+//         title: 'Encyclopedia',
+//         date: '2024-06-25',
+//         hasDetailPage: true
+//     },
+//     {
+//         key: 'F',
+//         title: 'Fortune',
+//         date: '2024-06-12',
+//         hasDetailPage: true
+//     },
+//     {
+//         key: 'G',
+//         title: '',//'Geek',
+//         hasDetailPage: false
+//     },
+//     {
+//         key: 'H',
+//         title: 'Homebody',
+//         date: '2024-05-24',
+//         hasDetailPage: true
+//     },
+//     {
+//         key: 'I',
+//         title: '', //'Introvert',
+//         hasDetailPage: false
+//     },
+//     {
+//         key: 'J',
+//         title: 'mbti-J',
+//         date: '2024-07-11',
+//         hasDetailPage: true
+//     },
+//     {
+//         key: 'K',
+//         title: 'Kidult',
+//         date: '2024-06-14',
+//         hasDetailPage: true
+//     },
+//     {
+//         key: 'L',
+//         title: '',
+//         hasDetailPage: false
+//     },
+//     {
+//         key: 'M',
+//         title: 'Mentor',
+//         date: '2024-07-25',
+//         hasDetailPage: true
+//     },
+//     {
+//         key: 'N',
+//         title: '',
+//         hasDetailPage: false
+//     },
+//     {
+//         key: 'O',
+//         title: '',
+//         hasDetailPage: false
+//     },
+//     {
+//         key: 'P',
+//         title: 'Postgraduate',
+//         date: '2024-05-30',
+//         hasDetailPage: true
+//     },
+//     {
+//         key: 'Q',
+//         title: '',
+//         hasDetailPage: false
+//     },
+//     {
+//         key: 'R',
+//         title: 'Rambler',
+//         date: '2024-07-30',
+//         hasDetailPage: true
+//     },
+//     {
+//         key: 'S',
+//         title: 'Steady',
+//         date: '2024-08-XX',
+//         hasDetailPage: true
+//     },
+//     {
+//         key: 'T',
+//         title: 'Tracing',
+//         date: '2024-07-18',
+//         hasDetailPage: true
+//     },
+//     {
+//         key: 'U',
+//         title: '',
+//         hasDetailPage: false
+//     },
+//     {
+//         key: 'V',
+//         title: 'Versatile',
+//         date: '2024-06-01',
+//         hasDetailPage: true
+//     },
+//     {
+//         key: 'W',
+//         title: 'Workaholic',
+//         date: '2024-07-16',
+//         hasDetailPage: true
+//     },
+//     {
+//         key: 'X',
+//         title: '',
+//         hasDetailPage: false
+//     },
+//     {
+//         key: 'Y',
+//         title: '',// 'YOLO',
+//         hasDetailPage: false
+//     },
+//     {
+//         key: 'Z',
+//         title: '', //'Zzz...',
+//         hasDetailPage: false
+//     },
+// ];
 
 export default items;
